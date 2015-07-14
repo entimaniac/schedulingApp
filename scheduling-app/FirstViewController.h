98491ee00c779b867e2c7059ae81b43091856fbc
Justin Willis <Justin@SunGeek.com>
2015-07-13 23:08:03 -0400

    Began working on Multipeer Connectivity. Does not work. Needs additional work.

--- scheduling-app/FirstViewController.h
+++ scheduling-app/FirstViewController.h
@@ -8,8 +8,17 @@
 
 #import <UIKit/UIKit.h>
 
-@interface FirstViewController : UIViewController
+@interface FirstViewController : UIViewController <MCSessionDelegate, MCBrowserViewControllerDelegate>
 
+@property (nonatomic, strong) MCPeerID *peerID;
+@property (nonatomic, strong) MCSession *session;
+@property (nonatomic, strong) MCBrowserViewController *browser;
+@property (nonatomic, strong) MCAdvertiserAssistant *advertiser;
+@property (nonatomic, strong) NSProgress *progress;
+
+-(void)setupPeerAndSessionWithDisplay:(NSString *)displayName;
+-(void)setupMCBrowser;
+-(void)advertiseSelf:(BOOL)shouldAdvertise;
 
 @end
 
