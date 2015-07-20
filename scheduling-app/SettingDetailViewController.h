c832f6d1373212c31f3a1f14ca3f9fddf990ca7d
Justin Willis <Justin@SunGeek.com>
2015-07-20 06:47:11 -0400

    Initial Commit

---
+++ scheduling-app/SettingDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingDetailViewController.h
+//  scheduling-app
+//
+//  Created by Mac user on 7/20/15.
+//  Copyright (c) 2015 com.justin. All rights reserved.
+//
+
+@import UIKit;
+
+@interface SettingDetailViewController : UITableViewController
+
+@end
