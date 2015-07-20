62e1e7958681daeac2e0b1ca8bc205a9fc8d3faf
Justin Willis <Justin@SunGeek.com>
2015-07-19 23:57:50 -0400

    Initial Commit

---
+++ scheduling-app/SettingsViewController.m
@@ -0,0 +1,14 @@
+//
+//  SettingsViewController.m
+//  scheduling-app
+//
+//  Created by Mac user on 7/19/15.
+//  Copyright (c) 2015 com.justin. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+@interface SettingsViewController()
+@end
+
+@implementation SettingsViewController
+@end
